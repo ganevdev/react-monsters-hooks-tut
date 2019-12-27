a4988f1b167baef6765edf9a9821dd87445d8f39
Ivan Ganev <iganevru@gmail.com>
2019-12-28 00:59:25 +0300

    ref

--- src/SearchParams/SearchParams.js
+++ src/SearchParams/SearchParams.js
@@ -1,24 +1,40 @@
 import './SearchParams.css';
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 import CardList from '../CardList';
 
+const requestUsers = async () => {
+  try {
+    let users = await fetch('https://jsonplaceholder.typicode.com/users');
+    users = await users.json();
+    return users;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 const SearchParams = () => {
   const [search, setSearch] = useState('');
+  const componentIsMounted = useRef(true);
   const [monsters, setMonsters] = useState([]);
-
-  const requestUsers = async () => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(users => setMonsters(users));
-  };
 
   const filterMonsters = (monsters, search = '') =>
     monsters.filter(m => m.name.toLowerCase().includes(search.toLowerCase()));
 
   useEffect(() => {
-    requestUsers();
+    requestUsers()
+      .then(users => {
+        if (componentIsMounted.current) setMonsters(users);
+      })
+      .catch(error => console.error(error));
+    // cleanup function, in which we are setting this component as unmounted
+    // this is an equivalent to componentWillUnmount class-based components
+    return () => (componentIsMounted.current = false);
+    //
+    // old
+    // requestUsers().then(usersFromApi => setMonsters(usersFromApi));
+    //
   }, []);
 
   return (
