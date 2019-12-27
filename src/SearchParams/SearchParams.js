63651f2f0aae8f52408d4e87fc0d617cb24a5385
Ivan Ganev <iganevru@gmail.com>
2019-12-28 01:00:07 +0300

    comment

--- src/SearchParams/SearchParams.js
+++ src/SearchParams/SearchParams.js
@@ -4,6 +4,7 @@ import React, { useEffect, useRef, useState } from 'react';
 
 import CardList from '../CardList';
 
+// this function can be anywere
 const requestUsers = async () => {
   try {
     let users = await fetch('https://jsonplaceholder.typicode.com/users');
